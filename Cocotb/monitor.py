ddc9e06b713741d063850dece3c0f55cc350a53f
Matt Hartnett <matthew.e.hartnett@gmail.com>
2024-01-19 16:42:54 -0700

    Reformat

--- Cocotb/monitor.py
+++ Cocotb/monitor.py
@@ -1,15 +1,14 @@
 import cocotb
-from cocotb.triggers import Timer
 from cocotb.triggers import RisingEdge
-from cocotb.triggers import FallingEdge
 
-class monitor:
+
+class Monitor:
     def __init__(self, clk, datas, valid):
         self._values = []
         self._clk = clk
-        self._datas = datas # Dictionary of signal name : handler pairs
+        self._datas = datas  # Dictionary of signal name : handler pairs
         self._valid = valid
-        self._coro = None # Coroutine handler
+        self._coro = None  # Coroutine handler
 
     def start(self):
         if self._running is not None:
@@ -22,14 +21,14 @@ class monitor:
         self._coro.kill()
         self._coro = None
 
-
     async def _run(self):
         while True:
-            await RisingEdge(self._clk) # Capture only on the rising edge
+            await RisingEdge(self._clk)  # Capture only on the rising edge
             if self._valid.value != 1:
                 await RisingEdge(self._valid)
             else:
                 self._values.append(self._sample())
+
     def _sample(self):
         sample_dict = {}
         for i in self._datas:
--- Cocotb/testbench.py
+++ Cocotb/testbench.py
@@ -2,37 +2,40 @@ import cocotb
 import monitor
 from cocotb.triggers import Timer
 
+
 async def reset_dut(reset_n, duration_ns):
     reset_n.value = 0
     await Timer(duration_ns, units="ns")
     reset_n.value = 1
     reset_n._log.info("Reset complete")
 
+
 async def generate_clock(clk, period):
     for cycle in range(1000):
         clk.value = 0
-        await Timer(period/2, units="ns")
+        await Timer(period / 2, units="ns")
         clk.value = 1
-        await Timer(period/2, units="ns")
-        
+        await Timer(period / 2, units="ns")
+
+
 @cocotb.test()
 async def testbench(dut):
-    period = 10 # ns
+    period = 10  # ns
     # Establish initial values for the input signals
     dut.CP.value = 1
     dut.P.value = 13
     dut.CEP.value = 1
     dut.CET.value = 1
     dut.PE.value = 1
-    
+
     # Tie the SR signal to the reset_n variable then run the blocking subroutine that executes a reset
     reset_n = dut.SR
     await reset_dut(reset_n, 100)
-    
+
     # Run the clock generate thread concurrently to the rest of the program
     clk = dut.CP
     clk_thread = cocotb.start_soon(generate_clock(clk, period))
-    
+
     for control in range(4):
         match control:
             case 0:
