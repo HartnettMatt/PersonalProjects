afe201e0bc06b8d2f35c8f98d5ae318698669b65
Matt Hartnett <matthew.e.hartnett@gmail.com>
2025-05-09 15:31:35 -0600

    Added IRSender project, updated gitignore for better MacOS development

---
+++ IRSender/IRSender.ino
@@ -0,0 +1,65 @@
+/*
+ * SimpleSender.cpp
+ *
+ *  Demonstrates sending IR codes in standard format with address and command
+ *  An extended example for sending can be found as SendDemo.
+ *
+ *  Copyright (C) 2020-2022  Armin Joachimsmeyer
+ *  armin.joachimsmeyer@gmail.com
+ *
+ *  This file is part of Arduino-IRremote https://github.com/Arduino-IRremote/Arduino-IRremote.
+ *
+ *  MIT License
+ */
+#include <Arduino.h>
+
+#if !defined(ARDUINO_ESP32C3_DEV) // This is due to a bug in RISC-V compiler, which requires unused function sections :-(.
+#define DISABLE_CODE_FOR_RECEIVER // Disables static receiver code like receive timer ISR handler and static IRReceiver and irparams data. Saves 450 bytes program memory and 269 bytes RAM if receiving functions are not required.
+#endif
+//#define SEND_PWM_BY_TIMER         // Disable carrier PWM generation in software and use (restricted) hardware PWM.
+//#define USE_NO_SEND_PWM           // Use no carrier PWM, just simulate an active low receiver signal. Overrides SEND_PWM_BY_TIMER definition
+
+/*
+ * This include defines the actual pin number for pins like IR_RECEIVE_PIN, IR_SEND_PIN for many different boards and architectures
+ */
+#include "PinDefinitionsAndMore.h"
+#include <IRremote.hpp> // include the library
+
+uint8_t cmd1 = 0x45;
+uint8_t cmd2 = 0x09;
+uint8_t sRepeats = 4;
+
+void setup() {
+    pinMode(LED_BUILTIN, OUTPUT);
+
+    Serial.begin(115200);
+    while (!Serial)
+        ; // Wait for Serial to become available. Is optimized away for some cores.
+
+    // Just to know which program is running on my Arduino
+    Serial.println(F("START " __FILE__ " from " __DATE__ "\r\nUsing library version " VERSION_IRREMOTE));
+    Serial.print(F("Send IR signals at pin "));
+    Serial.println(IR_SEND_PIN);
+
+    /*
+     * The IR library setup. That's all!
+     */
+    IrSender.begin(); // Start with IR_SEND_PIN -which is defined in PinDefinitionsAndMore.h- as send pin and enable feedback LED at default feedback LED pin
+    disableLEDFeedback(); // Disable feedback LED at default feedback LED pin
+    delay(500);
+    IrSender.sendNEC(0x00, cmd1, sRepeats);
+    delay(100);
+    IrSender.sendNEC(0x00, cmd2, sRepeats);
+    delay(500);
+}
+
+/*
+ * Set up the data to be sent.
+ * For most protocols, the data is build up with a constant 8 (or 16 byte) address
+ * and a variable 8 bit command.
+ * There are exceptions like Sony and Denon, which have 5 bit address.
+ */
+
+void loop() {
+  delay(1000);
+}
